67d0db0966a9b6c582fd3705a0ec7c269dfe19f7
MaikolSoro <maikolsoro.z1998@gmail.com>
2024-04-07 18:57:24 -0600

    Add explore view and listingitemview

---
+++ AirbnbClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  AirbnbClone
+//
+//  Created by Michael Soro on 7/4/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
