2141739c0ccb3527ebd50bde6a481581597c369d
MaikolSoro <maikolsoro.z1998@gmail.com>
2024-04-07 19:34:09 -0600

    Add search and filter

---
+++ AirbnbClone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterBar.swift
+//  AirbnbClone
+//
+//  Created by Michael Soro on 7/4/24.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
