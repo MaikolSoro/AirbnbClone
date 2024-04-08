a1fbf1eea1ca0586a61268001318cac556b27ebc
MaikolSoro <maikolsoro.z1998@gmail.com>
2024-04-07 22:18:48 -0600

    Add ListingDetailv1

---
+++ AirbnbClone/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailView.swift
+//  AirbnbClone
+//
+//  Created by Michael Soro on 7/4/24.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
